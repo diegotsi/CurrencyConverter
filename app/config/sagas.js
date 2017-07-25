087659b0b88035d16e01b3b6025a561e5568bf89
Diego Oliveira <diegooliveira.tsi@gmail.com>
2017-07-24 23:10:57 -0300

    Consulting Fixer.io api to get the latest conversion

--- app/config/sagas.js
+++ app/config/sagas.js
@@ -1,19 +1,36 @@
-import { takeEvery } from "redux-saga/effects";
-// 1. Swap currency
-// 2. Change base currency
-// 3. Upon initial app load
+import { takeEvery, call, put, select } from "redux-saga/effects";
 
 import {
-  SWAP_CURRENCY,
   CHANGE_BASE_CURRENCY,
-  GET_INITIAL_CONVERSION
+  GET_INITIAL_CONVERSION,
+  SWAP_CURRENCY,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR
 } from "../actions/currencies";
 
+export const getLatestRate = currency =>
+  fetch(`http://api.fixer.io/latest?base=${currency}`);
+
 function* fetchLatestConversionRates(action) {
-  console.log("TODO: Update the things", action);
-  yield;
+  try {
+    let currency = action.currency;
+    if (currency === undefined) {
+      currency = yield select(state => state.currencies.baseCurrency);
+    }
+    const response = yield call(getLatestRate, currency);
+    const result = yield response.json();
+    if (result.error) {
+      yield put({ type: CONVERSION_ERROR, error: result.error });
+    } else {
+      yield put({ type: CONVERSION_RESULT, result });
+    }
+  } catch (error) {
+    yield put({ type: CONVERSION_ERROR, error: error.message });
+  }
 }
 
 export default function* rootSaga() {
   yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates);
+  yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates);
+  yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates);
 }
--- app/reducers/currencies.js
+++ app/reducers/currencies.js
@@ -3,15 +3,17 @@ import {
   SWAP_CURRENCY,
   CHANGE_BASE_CURRENCY,
   CHANGE_QUOTE_CURRENCY,
-  swapCurrency,
-  changeCurrencyAmount
+  GET_INITIAL_CONVERSION,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR
 } from "../actions/currencies";
 
 const initialState = {
   baseCurrency: "USD",
   quoteCurrency: "GBP",
   amount: 100,
-  conversions: {}
+  conversions: {},
+  error: null
 };
 
 const setConversions = (state, action) => {
@@ -31,13 +33,10 @@ const setConversions = (state, action) => {
   };
 };
 
-const reducer = (state = initialState, action) => {
+export default (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_CURRENCY_AMOUNT:
-      return {
-        ...state,
-        amount: action.amount || 0
-      };
+      return { ...state, amount: action.amount || 0 };
     case SWAP_CURRENCY:
       return {
         ...state,
@@ -56,9 +55,26 @@ const reducer = (state = initialState, action) => {
         quoteCurrency: action.currency,
         conversions: setConversions(state, action)
       };
+    case GET_INITIAL_CONVERSION:
+      return {
+        ...state,
+        conversions: setConversions(state, { currency: state.baseCurrency })
+      };
+    case CONVERSION_RESULT:
+      return {
+        ...state,
+        baseCurrency: action.result.base,
+        conversions: {
+          ...state.conversions,
+          [action.result.base]: {
+            isFetching: false,
+            ...action.result
+          }
+        }
+      };
+    case CONVERSION_ERROR:
+      return { ...state, error: action.error };
     default:
       return state;
   }
 };
-
-export default reducer;
